0c0d0601396391ebe571c43407cccd9dd6d89ee8
LookinGit <alook.in@yandex.ru>
2018-11-16 19:51:28 +0500

    refactor(sao): update sao file

---
+++ saofile.js
@@ -0,0 +1,31 @@
+module.exports = {
+  prompts () {
+    return [
+      {
+        name: 'name',
+        message: 'Project name',
+        default: '{outFolder}'
+      },
+      {
+        name: 'description',
+        message: 'Project description',
+        default: `My PWA App`
+      }
+    ]
+  },
+
+  actions: [
+    {
+      type: 'move',
+      patterns: {
+        'gitignore': '.gitignore'
+      }
+    }
+  ],
+
+  async completed () {
+    this.gitInit()
+    await this.npmInstall()
+    this.showCompleteTips()
+  }
+}
